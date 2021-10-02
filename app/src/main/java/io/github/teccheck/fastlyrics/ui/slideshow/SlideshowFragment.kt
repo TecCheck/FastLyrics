010d5098d9bddf18162101231ae4066432a64301
TecCheck <mcteccy@protonmail.com>
2021-10-02 22:30:01 +0200

    Add Navigation Drawer Activity as MainActivity

---
+++ app/src/main/java/io/github/teccheck/fastlyrics/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,45 @@
+package io.github.teccheck.fastlyrics.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import io.github.teccheck.fastlyrics.R
+import io.github.teccheck.fastlyrics.databinding.FragmentSlideshowBinding
+
+class SlideshowFragment : Fragment() {
+
+    private lateinit var slideshowViewModel: SlideshowViewModel
+    private var _binding: FragmentSlideshowBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        slideshowViewModel =
+            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+
+        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

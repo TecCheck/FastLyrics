41dfe8fc6ac3fdb2cafa38a08e909155f525e1f8
TecCheck <mcteccy@protonmail.com>
2021-10-08 17:51:34 +0200

    Tweak fragments

--- app/src/main/java/io/github/teccheck/fastlyrics/ui/lyrics/LyricsFragment.kt
+++ app/src/main/java/io/github/teccheck/fastlyrics/ui/lyrics/LyricsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import io.github.teccheck.fastlyrics.databinding.FragmentLyricsBinding
@@ -24,13 +23,11 @@ class LyricsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         lyricsViewModel = ViewModelProvider(this).get(LyricsViewModel::class.java)
-
         _binding = FragmentLyricsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        lyricsViewModel.text.observe(viewLifecycleOwner, { textView.text = it })
-        return root
+        lyricsViewModel.text.observe(viewLifecycleOwner, { binding.textLyrics.text = it })
+
+        return binding.root;
     }
 
     override fun onDestroyView() {
--- app/src/main/java/io/github/teccheck/fastlyrics/ui/saved/SavedFragment.kt
+++ app/src/main/java/io/github/teccheck/fastlyrics/ui/saved/SavedFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import io.github.teccheck.fastlyrics.databinding.FragmentSavedBinding
 
 class SavedFragment : Fragment() {
 
-    private lateinit var savedViewModel: SavedViewModel
     private var _binding: FragmentSavedBinding? = null
 
     // This property is only valid between onCreateView and
@@ -23,14 +20,8 @@ class SavedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        savedViewModel = ViewModelProvider(this).get(SavedViewModel::class.java)
-
         _binding = FragmentSavedBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        savedViewModel.text.observe(viewLifecycleOwner, { textView.text = it })
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {

0eb8c1be93b3b1cdb7f9c621cc21f1eb538e6738
Teccheck <mcteccy@protonmail.com>
2024-10-27 19:17:18 +0100

    fix(model): implement equals and hashCode for SongMeta

--- app/src/main/java/io/github/teccheck/fastlyrics/model/SongMeta.kt
+++ app/src/main/java/io/github/teccheck/fastlyrics/model/SongMeta.kt
@@ -12,4 +12,28 @@ data class SongMeta(
     override fun toString(): String {
         return "SongMeta(title='$title', artist=$artist, album=$album, art=$art, duration=$duration)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SongMeta
+
+        if (title != other.title) return false
+        if (artist != other.artist) return false
+        if (album != other.album) return false
+        // Art is left out
+        if (duration != other.duration) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + (artist?.hashCode() ?: 0)
+        result = 31 * result + (album?.hashCode() ?: 0)
+        result = 31 * result + (art?.hashCode() ?: 0)
+        result = 31 * result + (duration?.hashCode() ?: 0)
+        return result
+    }
 }

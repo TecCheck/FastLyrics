feafe2398bb04aa595bf48f5d0cc1d679f61475c
TecCheck <mcteccy@protonmail.com>
2024-11-19 09:44:01 +0100

    refactor: clean up ViewLyricsActivity

--- app/src/main/java/io/github/teccheck/fastlyrics/utils/Utils.kt
+++ app/src/main/java/io/github/teccheck/fastlyrics/utils/Utils.kt
@@ -2,6 +2,7 @@ package io.github.teccheck.fastlyrics.utils
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.View
@@ -9,7 +10,6 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.app.ShareCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Success
@@ -38,30 +38,26 @@ object Utils {
         visibility = if (visible) View.VISIBLE else View.GONE
     }
 
-    fun Fragment.copyToClipboard(title: String, text: String) {
-        val clipboard =
-            ContextCompat.getSystemService(requireContext(), ClipboardManager::class.java)
-        val clip = ClipData.newPlainText(title, text)
-        clipboard?.setPrimaryClip(clip)
-    }
+    fun copyToClipboard(context: Context, title: String, text: String) =
+        ContextCompat.getSystemService(context, ClipboardManager::class.java)
+            ?.setPrimaryClip(ClipData.newPlainText(title, text))
 
-    fun Fragment.openLink(link: String) = startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
+    fun openLink(context: Context, link: String) =
+        context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
 
-    fun Fragment.share(songTitle: String, artist: String, text: String) {
-        val title = getString(R.string.share_title, songTitle, artist)
-        ShareCompat.IntentBuilder(requireContext()).setText(text).setType("text/plain")
+    fun share(context: Context, songTitle: String, artist: String, text: String) {
+        val title = context.getString(R.string.share_title, songTitle, artist)
+        ShareCompat.IntentBuilder(context).setText(text).setType("text/plain")
             .setChooserTitle(title).setSubject(title).startChooser()
     }
 
     fun SongWithLyrics.getLyrics(preferSynced: Boolean = false): String {
-        if (preferSynced && lyricsSynced != null)
-            return lyricsSynced
+        if (preferSynced && lyricsSynced != null) return lyricsSynced
 
-        if (lyricsPlain != null)
-            return lyricsPlain
+        if (lyricsPlain != null) return lyricsPlain
 
-        if (lyricsSynced != null)
-            return SyncedLyrics.parseLrcToList(lyricsSynced).joinToString("\n") { it.text }
+        if (lyricsSynced != null) return SyncedLyrics.parseLrcToList(lyricsSynced)
+            .joinToString("\n") { it.text }
 
         return ""
     }
